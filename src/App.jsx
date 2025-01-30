72445ca435df389ce36f2691e0f12b8da128e294
Dilyorbek Ubaydullayev <dilyorbekubaydullayev@gmail.com>
2025-01-30 21:32:12 +0500

    ozgarish2

--- src/App.jsx
+++ src/App.jsx
@@ -646,7 +646,7 @@ function App() {
                         value={opt}
                         checked={userAnswers[index] === opt}
                         onChange={() => handleOptionChange(index, opt)}
-                        className="mt-1 xs:mt-0"
+                        className="mt-1 xs:-mt-1"
                       />
                       <span className="text-sm">{opt}</span>
                     </label>
