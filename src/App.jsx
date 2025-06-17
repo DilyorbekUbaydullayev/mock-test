161c7a5e622614100420af2daa74cd00b7c8a81c
Dilyorbek Ubaydullayev <dilyorbekubaydullayev@gmail.com>
2025-06-18 04:40:40 +0500

    hisob

--- src/App.jsx
+++ src/App.jsx
@@ -255,12 +255,12 @@ function App() {
 
               </div>
               <div>
-                <a href="/Savollar ingliz tili yakuniy.pdf">
+                <a href="/Hisob2 test.pdf">
                 <label 
                   
                   className="block w-full px-4 py-2 text-center border-2 border-blue-500 rounded-lg hover:bg-blue-50 cursor-pointer"
                 >
-                  Inglis tili javoblar
+                  hisob javoblar
                 </label>
                 </a>
               </div>
